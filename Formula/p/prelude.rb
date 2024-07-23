d7372171fa8e369e4cdb3227433358b04ca867e8
aerugo <hugiasgeirsson@gmail.com>
2024-07-23 20:41:11 +0200

    Added Prelude

---
+++ Formula/p/prelude.rb
@@ -0,0 +1,17 @@
+class Prelude < Formula
+  desc "Tool to generate prompts containing file tree and concatenated file contents"
+  homepage "https://github.com/aerugo/prelude"
+  url "https://github.com/aerugo/prelude/archive/refs/tags/v0.3.tar.gz"
+  sha256 "ba1cc4ec19375707603da30ce5dec516e6171593f649b7d26fe327cb608c0f20"
+  license "MIT"
+
+  depends_on "tree"
+
+  def install
+    bin.install "prelude"
+  end
+
+  test do
+    system "#{bin}/prelude", "--help"
+  end
+end
